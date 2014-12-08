d3c38c9648fc71b76b0c507beb5c6dd2f8f4d5f8
Michal Ogrodniczak <mjogrodniczak@gmail.com>
2014-12-08 11:32:47 +0000

    polished up the feed, added reports

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User
   include Mongoid::Document
   include Mongoid::Timestamps
   include ActiveModel::SecurePassword
+  include Mongoid::Paperclip
 
   field :first_name, type: String
   field :last_name, type: String
@@ -13,9 +14,20 @@ class User
   field :remember_token, type: String
   has_secure_password
 
+  has_mongoid_attached_file :photo,
+                            :styles => {
+                                :thumb => "100x100#",
+                                :small  => "150x150>",
+                                :medium => "200x200" },
+      :default_url => "silhouette.jpg"
+
+  validates_attachment :photo, content_type: { content_type: ['image/jpg', 'image/jpeg','image/png', 'image/gif'] }
+
   has_many :activities, dependent: :destroy
   has_and_belongs_to_many :following, class_name: 'User', inverse_of: :followers, autosave: true
   has_and_belongs_to_many :followers, class_name: 'User', inverse_of: :following
+
+
 
   validates :password, presence: true, length: {minimum: 6}
   validates :password_confirmation, presence: true
